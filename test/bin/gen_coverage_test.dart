75b4a1796b0c8cee861faeb021cfaf4da0c47a5a
civts <giacomo.zanolli@studenti.unitn.it>
2021-05-04 17:55:03 +0200

    Add test for detection of invalid min-coverage
    Add some tests to ensure that we check that the value passed as
    'min-coverage' option is what we expect (an integer between 0 and 100,
     inclusive)

--- test/bin/gen_coverage_test.dart
+++ test/bin/gen_coverage_test.dart
@@ -30,6 +30,53 @@ void main() {
           expect(exitCode, 1);
         });
       });
+      group('if min-coverage argument is', () {
+        const expectedErrorMessage = 'invalid min-coverage supplied\n'
+            'it must be an integer between 0 and 100\n';
+        group('not an integer', () {
+          test('should report the error to the user', () async {
+            const invalid = ['', 'null', '20.0', 'string'];
+            for (final i in invalid) {
+              final process = await Process.start(
+                'dart',
+                ['run', 'gen_coverage', '--min-coverage', i],
+              );
+              await expectLater(
+                process.stdout.transform(utf8.decoder),
+                emits(expectedErrorMessage),
+              );
+              final exitCode = await process.exitCode;
+              expect(exitCode, 1);
+            }
+          });
+        });
+        group('not in the acceptable range should error', () {
+          test('less than 0', () async {
+            final process = await Process.start(
+              'dart',
+              ['run', 'gen_coverage', '--min-coverage', '-1'],
+            );
+            await expectLater(
+              process.stdout.transform(utf8.decoder),
+              emits(expectedErrorMessage),
+            );
+            final exitCode = await process.exitCode;
+            expect(exitCode, 1);
+          });
+          test('greater than 100', () async {
+            final process = await Process.start(
+              'dart',
+              ['run', 'gen_coverage', '--min-coverage', '101'],
+            );
+            await expectLater(
+              process.stdout.transform(utf8.decoder),
+              emits(expectedErrorMessage),
+            );
+            final exitCode = await process.exitCode;
+            expect(exitCode, 1);
+          });
+        });
+      });
     });
   });
 }
